5c8156771e912fea226a5c5fe3ae81285afaab9e
Paddy Cooper <1210@rgsg.co.uk>
2021-08-31 10:21:13 +0100

    Added error messages

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -55,6 +55,16 @@ class JoinRoom(APIView):
                     },
                     status=status.HTTP_200_OK,
                 )
+            return Response(
+                {
+                    "Bad Request": "Invalid room code",
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        return Response(
+            {"Bad Request": "Invalid post data, did not find a code key"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 class CreateRoomView(APIView):
@@ -75,6 +85,9 @@ class CreateRoomView(APIView):
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
                 room.save(update_fields=["guest_can_pause", "votes_to_skip"])
+                self.request.session["room_code"] = room.code
+
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
 
             else:
                 room = Room(
@@ -83,5 +96,11 @@ class CreateRoomView(APIView):
                     votes_to_skip=votes_to_skip,
                 )
                 room.save()
+                self.request.session["room_code"] = room.code
+                return Response(
+                    RoomSerializer(room).data, status=status.HTTP_201_CREATED
+                )
 
-            return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+        return Response(
+            {"Bad Request": "Invalid data..."}, status=status.HTTP_400_BAD_REQUEST
+        )
