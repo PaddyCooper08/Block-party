188c8027c058ae2bd8e02550d5cc2eea6c170757
Paddy Cooper <1210@rgsg.co.uk>
2021-08-31 11:39:21 +0100

    Added userinroom to urls

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path("create-room", CreateRoomView.as_view()),
     path("get-room", GetRoom.as_view()),
     path("join-room", JoinRoom.as_view()),
+    path("user-in-room", UserInRoom.as_view()),
 ]
