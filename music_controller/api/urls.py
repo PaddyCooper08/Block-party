51756694b9300ea1d726c139e62dcc93c4e82aa4
Paddy Cooper <1210@rgsg.co.uk>
2021-09-11 12:29:01 +0100

    Added leave room endpoint

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom
+from .views import RoomView, CreateRoomView, GetRoom, JoinRoom, UserInRoom, LeaveRoom
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path("get-room", GetRoom.as_view()),
     path("join-room", JoinRoom.as_view()),
     path("user-in-room", UserInRoom.as_view()),
+    path("leave-room", LeaveRoom.as_view()),
 ]
