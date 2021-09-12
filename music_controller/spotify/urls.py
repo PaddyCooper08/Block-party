70c5a197905775f4ea21e617802e3a5b3ad5b3c9
Paddy Cooper <1210@rgsg.co.uk>
2021-09-12 16:09:30 +0100

    Created endpoint for IsAuthenticated

--- music_controller/spotify/urls.py
+++ music_controller/spotify/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AuthURL, spotify_callback
+from .views import AuthURL, IsAuthenticated, spotify_callback, 
 
 urlpatterns = [
     path("get-auth-url", AuthURL.as_view()),
     path("redirect", spotify_callback),
+    path('is-authenticated', IsAuthenticated.as_view())
+
 ]
