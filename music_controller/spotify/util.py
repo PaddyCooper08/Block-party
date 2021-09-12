6d59f2cd87d8e3cc62b9d324977d7214358c417c
Paddy Cooper <1210@rgsg.co.uk>
2021-09-12 16:05:17 +0100

    Created is_spotify_authenticated and refresh_spotify_token functions

--- music_controller/spotify/util.py
+++ music_controller/spotify/util.py
@@ -1,6 +1,8 @@
 from .models import SpotifyToken
 from django.utils import timezone
 from datetime import timedelta
+from requests import post
+from .credentials import CLIENT_ID, CLIENT_SECRET
 
 
 def get_user_tokens(session_id):
@@ -34,3 +36,37 @@ def update_or_create_user_tokens(
             expires_in=expires_in,
         )
         tokens.save()
+
+
+def is_spotify_authenticated(session_id):
+    tokens = get_user_tokens(session_id)
+    if tokens:
+        expiry = tokens.expires_in
+        if expiry <= timezone.now():
+            refresh_spotify_token(session_id)
+        return True
+
+    return False
+
+
+def refresh_spotify_token(session_id):
+    refresh_token = get_user_tokens(session_id).refresh_token
+
+    response = post(
+        "https://accounts.spotify.com/api/token",
+        data={
+            "grant_type": "refresh_token",
+            "refresh_token": refresh_token,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+        },
+    ).json()
+
+    access_token = response.get("access_token")
+    token_type = response.get("token_type")
+    expires_in = response.get("expires_in")
+    refresh_token = response.get("refresh_token")
+
+    update_or_create_user_tokens(
+        session_id, access_token, token_type, expires_in, refresh_token
+    )
