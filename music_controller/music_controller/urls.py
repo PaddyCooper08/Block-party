d42544c4143e20c50340265da42bb861fa9fd016
Paddy Cooper <1210@rgsg.co.uk>
2021-09-12 15:16:58 +0100

    Included spotify.urls

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
     path("", include("frontend.urls")),
+    path("spotify/", include("spotify.urls")),
 ]
