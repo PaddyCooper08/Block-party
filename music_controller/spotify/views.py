96064b446f2ff110006b698928d7033f3c388784
Paddy Cooper <1210@rgsg.co.uk>
2021-09-12 15:11:09 +0100

    Created AuthURL view

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from requests.models import Response
+from .credentials import REDIRECT_URI, CLIENT_SECRET, CLIENT_ID
+from rest_framework.views import APIView
+from requests import Request, post
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+class AuthURL(APIView):
+    def get(self, request, fornat=None):
+        scopes = "user-read-playback-state user-modify-playback-state user-read-currently-playing"
+
+        url = (
+            Request(
+                "GET",
+                "https://accounts.spotify.com/authorize",
+                params={
+                    "scope": scopes,
+                    "response_type": "code",
+                    "redirect_uri": REDIRECT_URI,
+                    "client_id": CLIENT_ID,
+                },
+            )
+            .prepare()
+            .url
+        )
+
+        return Response({"url": url}, status=status.HTTP_200_OK)
