e4c55cb87c80ce1025d4c4fb3816285f9dc858a3
Paddy Cooper <1210@rgsg.co.uk>
2021-09-12 16:08:24 +0100

    Created IsAuthenticated class

--- music_controller/spotify/views.py
+++ music_controller/spotify/views.py
@@ -5,7 +5,11 @@ from rest_framework.views import APIView
 from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
-from .util import update_or_create_user_tokens
+from .util import (
+    is_spotify_authenticated,
+    update_or_create_user_tokens,
+    is_spotify_authenticated,
+)
 
 
 class AuthURL(APIView):
@@ -59,3 +63,9 @@ def spotify_callback(request, format=None):
     )
 
     return redirect("frontend:")
+
+
+class IsAuthenticated(APIView):
+    def get(self, request, format=None):
+        is_authenticated = is_spotify_authenticated(self.request.session.session_key)
+        return Response({"status": is_authenticated}, status=status.HTTP_200_OK)
