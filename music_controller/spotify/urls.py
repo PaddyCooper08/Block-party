f3b005ee06357d94e3591892a5ec5ca86e685a24
Paddy Cooper <1210@rgsg.co.uk>
2021-09-12 16:12:01 +0100

    Removed extra comma

--- music_controller/spotify/urls.py
+++ music_controller/spotify/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import AuthURL, IsAuthenticated, spotify_callback, 
+from .views import AuthURL, IsAuthenticated, spotify_callback
 
 urlpatterns = [
     path("get-auth-url", AuthURL.as_view()),
     path("redirect", spotify_callback),
-    path('is-authenticated', IsAuthenticated.as_view())
-
+    path("is-authenticated", IsAuthenticated.as_view()),
 ]
