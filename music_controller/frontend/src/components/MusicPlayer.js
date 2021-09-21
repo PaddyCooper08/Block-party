831fbd4484f6cb49f61f3f00eb6f151c25137a4d
Paddy Cooper <1210@rgsg.co.uk>
2021-09-21 20:23:49 +0100

    Created MusicPlayer.js

---
+++ music_controller/frontend/src/components/MusicPlayer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {
+  Grid,
+  Typography,
+  Card,
+  Iconbutton,
+  LinearProgress,
+} from "@material-ui/core";
+import { PlayArrowIcon, SkipNextIcon, PauseIcon } from "@material-ui/icons";
+
+export default class MusicPlayer extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {}
+}
