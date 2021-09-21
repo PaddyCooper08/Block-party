6efb7c7c08cba6dceaf26c6d22dd95f1a4b5e790
Paddy Cooper <1210@rgsg.co.uk>
2021-09-21 20:34:47 +0100

    Injected props into MusicPlayer.js

--- music_controller/frontend/src/components/Room.js
+++ music_controller/frontend/src/components/Room.js
@@ -7,6 +7,7 @@ import {
   IconButton,
 } from "@material-ui/core";
 import CreateRoomPage from "./CreateRoomPage";
+import MusicPlayer from "./MusicPlayer";
 
 export default class Room extends Component {
   constructor(props) {
@@ -161,6 +162,7 @@ export default class Room extends Component {
             Code: {this.roomCode}
           </Typography>
         </Grid>
+        <MusicPlayer {...this.state.song} />
 
         <Grid item xs={12} align="center">
           <Button
