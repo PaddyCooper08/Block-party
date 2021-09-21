23de067cf4c4110b1ce60af42fe0415bac141fd3
Paddy Cooper <1210@rgsg.co.uk>
2021-09-21 20:32:46 +0100

    Stylised page

--- music_controller/frontend/src/components/MusicPlayer.js
+++ music_controller/frontend/src/components/MusicPlayer.js
@@ -12,5 +12,33 @@ export default class MusicPlayer extends Component {
   constructor(props) {
     super(props);
   }
-  render() {}
+  render() {
+    const songProgress = (this.props.time / this.props.duration) * 100;
+    return (
+      <Card>
+        <Grid container alignItems="center">
+          <Grid item xs={4} align="center">
+            <img src={this.props.image_url} height="100%" width="100%" />
+          </Grid>
+          <Grid item xs={8} align="center">
+            <Typography component="h5" variant="h5">
+              {this.props.title}
+            </Typography>
+            <Typography color="textSecondary" variant="subtitle1">
+              {this.props.artist}
+            </Typography>
+            <div>
+              <IconButton>
+                {this.props.is_playing ? <PauseIcon /> : <PlayArrowIcon />}
+              </IconButton>
+              <IconButton>
+                <SkipNextIcon />
+              </IconButton>
+            </div>
+          </Grid>
+        </Grid>
+        <LinearProgress variant="determinate" value={songProgress} />
+      </Card>
+    );
+  }
 }
